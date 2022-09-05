a9a0535be796f96041164fbf83e13e609bdd4533
Bhavneet Singh <bhavneetsingh@Bhavneets-MacBook-Air.local>
2022-09-05 13:39:21 +0530

    added increaseQuantity functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            price: 999, 
+            title: 'phone', 
+            quantity: 1, 
+            img: ''  
+        }
+        this.increaseQuantity = this.increaseQuantity.bind(this)
+    }
+
+    increaseQuantity(){
+        // this.state.quantity += 1;
+        console.log('testing', this.state);
+        // this.state.quantity += 1;
+    }
+
     render(){
         return (
             <div className="cart-item">
@@ -8,11 +25,30 @@ class CartItem extends React.Component{
                     <img style = {styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs. 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize: 25}}>{this.state.title}</div>
+                    <div style={{color: '#777'}}>Rs. {this.state.price}</div>
+                    <div style={{color: '#777'}}>Qty: {this.state.quantity}</div>
                     <div className='cart-item-actions'>
+
                         {/* {Buttons} */}
+                        <img 
+                        alt='increase' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/992/992651.png'
+                        onClick={this.increaseQuantity}
+                        />
+                        <img 
+                        alt='decrease' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/992/992683.png' 
+                        // onClick={this.decreaseQuantity}
+                        />
+                        <img 
+                        alt='delete' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/64/64022.png' 
+                        />
+
                     </div>
                 </div>
             </div>
@@ -24,7 +60,8 @@ const styles = {
     image:{
         height: 110, 
         width: 100, 
-        borderRadius: 4
+        borderRadius: 4, 
+        // backgroundColor: 'gray'
     }
 }
 
